e1a5b204b8deed664256194f21bb4d3de698e488
mvchfvn <mimi123meg@gmail.com>
2018-07-23 16:32:43 +0000

    automatically backed up by learn

--- stock_supermarket.sh
+++ stock_supermarket.sh
@@ -6,4 +6,8 @@ mv delivery/brie.txt dairy
 mv delivery/broccoli.txt veggies
 mv delivery/carrot_cake.txt dessert
 mv delivery/carrots.txt veggies
-mv delivery/eggo_waffles.txt 
+mv delivery/eggo_waffles.txt snax
+mv delivery/fage_yogurt.txt dairy
+mv delivery/fig_bars.txt snax
+mv delivery/flourless_cake.txt dessert
+mv delivery/garlic.txt 
