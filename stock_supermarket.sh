c370c9896c8f368de312eddb5c2d9969ca05b8aa
mvchfvn <mimi123meg@gmail.com>
2018-07-23 16:36:50 +0000

    automatically backed up by learn

--- stock_supermarket.sh
+++ stock_supermarket.sh
@@ -12,4 +12,9 @@ mv delivery/fig_bars.txt snax
 mv delivery/flourless_cake.txt dessert
 mv delivery/garlic.txt veggies
 mv delivery/goldfish.txt meat
-mv delivery/grapples.txt 
+mv delivery/grapples.txt fruit
+mv delivery/hotdogs.txt meat
+mv delivery/icecream.txt dairy
+mv delivery/lamb_ribs.txt meat
+mv delivery/land_o_lakes_butter.txt dairy
+mv delivery/lean_cuisine.txt 
